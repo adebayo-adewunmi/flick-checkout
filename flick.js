fc4358d583b952faa04d8a260e4db5f391f1c84d
adebayo-adewunmi <adebayo.adewunmi@africaprudential.com>
2021-04-26 08:17:36 +0100

    minor update

--- flick.js
+++ flick.js
@@ -149,6 +149,7 @@ let Payment = {
 
 		document.getElementById("customer-account-pay-btn").innerHTML = "NGN"+paymentAmount;
 		document.getElementById("customer-paycard-btn").innerHTML = "NGN"+paymentAmount;
+		
 		document.getElementById("paid-customer-email").innerHTML = customerEmail;
 	}
 }
